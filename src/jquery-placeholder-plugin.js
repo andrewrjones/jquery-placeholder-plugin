182a5b236430d32a0b5d906b28d2c5d50c1d0b38
Mal Curtis <mal@sitepoint.com>
2010-11-19 16:54:45 +1100

    Fixed bug: If the value is actually set to the same as the placeholder, it's still considered a placeholder Fixed bug: Refresh in firefox pre fills the input field with the placeholder Fixed bug: Val() function returns the placeholder, not an empty string Fixed bug: Submitting a form would submit the placeholder value
    Feature: Optionally attempt to prevent FireFox from auto filling values on refresh, by turning autocomplete off. This can be turned off in the settings

--- src/jquery-placeholder-plugin.js
+++ src/jquery-placeholder-plugin.js
@@ -3,16 +3,42 @@
     $.support.placeholder = 'placeholder' in document.createElement('input');
 
     // plugin code
-    $.fn.placeholder = function(opts) {
-        opts = $.extend({
-			blurClass: 'placeholderBlur',
-			activeClass: 'placeholderFocus',
-			overrideSupport: false
+	$.fn.placeholder = function(opts) {
+		opts = $.extend({
+			focusClass: 'placeholderFocus',
+			activeClass: 'placeholderActive',
+			overrideSupport: false,
+			preventRefreshIssues: true
 		}, opts);
 
 		// we don't have to do anything if the browser supports placeholder
 		if(!opts.overrideSupport && $.support.placeholder)
 		    return this;
+		
+		// Replace the val function to never return placeholders
+		$.fn.plVal = $.fn.val;
+		$.fn.val = function(value) {
+			if(value != undefined)
+				return $(this).plVal(value);
+			
+
+			if(this[0]) {
+        		var el = $(this[0]);
+        		if(el.hasClass(opts.activeClass) && el.plVal() == el.attr('placeholder')) {
+           			return '';
+       			} else {
+           	 		return el.plVal();
+        		}
+    		}
+    		return undefined;
+		};
+		
+		// Clear placeholder values upon page reload
+		$(window).bind('beforeunload.placeholder', function() {
+			console.log('removing values');
+			$('input.' + opts.placeholderBlur).val('').attr('autocomplete','off');
+		});
+
 
         return this.each(function() {
             var $el = $(this);
@@ -24,22 +50,35 @@
             // we cannot do password fields, but supported browsers can
             if($el.is(':password'))
                 return;
+			
+			// Prevent values from being reapplied on refresh
+			if(opts.preventRefreshIssues):
+				$el.attr('autocomplete','off');
 
-            $el.focus(function(){
+            $el.bind('focus.placeholder', function(){
                 var $el = $(this);
-                if($el.val() == $el.attr('placeholder'))
+                if(this.value == $el.attr('placeholder') && $el.hasClass(opts.activeClass))
                     $el.val('')
-                       .removeClass(opts.blurClass)
-                       .addClass(opts.activeClass);
+                       .removeClass(opts.activeClass)
+                       .addClass(opts.focusClass);
             });
-            $el.blur(function(){
+            $el.bind('blur.placeholder', function(){
                 var $el = $(this);
-                if($el.val() == '')
+				
+				$el.removeClass(opts.focusClass);
+
+                if(this.value == '')
                   $el.val($el.attr('placeholder'))
-                     .removeClass(opts.activeClass)
-                     .addClass(opts.blurClass);
+                     .addClass(opts.activeClass);
             });
+
             $el.triggerHandler('blur');
+			
+			// Prevent incorrect form values being posted
+			$el.parents('form').submit(function(){
+				$el.triggerHandler('focus.placeholder');
+			});
+
         });
     };
 })(jQuery);
