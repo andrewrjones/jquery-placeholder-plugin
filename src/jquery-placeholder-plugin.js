6c13d7b38670aec30f38cb6a4fade1f1210ca8c3
Mal Curtis <mal@sitepoint.com>
2010-11-22 11:06:09 +1100

    hahaha I suck.

--- src/jquery-placeholder-plugin.js
+++ src/jquery-placeholder-plugin.js
@@ -1,51 +1,77 @@
 (function($) {
+	$.extend({
+		placeholder : {
+			settings : {
+				focusClass: 'placeholderFocus',
+				activeClass: 'placeholder',
+				overrideSupport: false,
+				preventRefreshIssues: true
+			},
+			debug : false,
+			log : function(msg){
+				if(!$.placeholder.debug) return;
+				msg = "[DirtyForms] " + msg;
+				$.placeholder.hasFirebug ?
+				console.log(msg) :
+				$.placeholder.hasConsoleLog ?
+					window.console.log(msg) :
+					alert(msg);
+			},
+			hasFirebug : "console" in window && "firebug" in window.console,
+			hasConsoleLog: "console" in window && "log" in window.console
+		}
+
+	});
+
     // check browser support for placeholder
     $.support.placeholder = 'placeholder' in document.createElement('input');
 
+	// Replace the val function to never return placeholders
+	$.fn.plVal = $.fn.val;
+	$.fn.val = function(value) {
+		$.placeholder.log('in val');
+		if(this[0]) {
+			$.placeholder.log('have found an element');
+			var el = $(this[0]);
+			if(value != undefined)
+			{
+				$.placeholder.log('in setter');
+				var currentValue = el.plVal();
+				var returnValue = $(this).plVal(value);
+				if(el.hasClass($.placeholder.settings.activeClass) && currentValue == el.attr('placeholder')){
+					el.removeClass($.placeholder.settings.activeClass);
+				}
+				return returnValue;
+			}
+
+			if(el.hasClass($.placeholder.settings.activeClass) && el.plVal() == el.attr('placeholder')) {
+				$.placeholder.log('returning empty because its a placeholder');
+				return '';
+			} else {
+				$.placeholder.log('returning original val');
+				return el.plVal();
+			}
+		}
+		$.placeholder.log('returning undefined');
+		return undefined;
+	};
+
+	// Clear placeholder values upon page reload
+	$(window).bind('beforeunload.placeholder', function() {
+		var els = $('input.placeholderActive' );
+		if(els.length > 0)
+			els.val('').attr('autocomplete','off');
+	});
+
+
     // plugin code
 	$.fn.placeholder = function(opts) {
-		opts = $.extend({
-			focusClass: 'placeholderFocus',
-			activeClass: 'placeholderActive',
-			overrideSupport: false,
-			preventRefreshIssues: true
-		}, opts);
+		opts = $.extend({},$.placeholder.settings, opts);
 
 		// we don't have to do anything if the browser supports placeholder
 		if(!opts.overrideSupport && $.support.placeholder)
 		    return this;
-		
-		// Replace the val function to never return placeholders
-		$.fn.plVal = $.fn.val;
-		$.fn.val = function(value) {
-		
-			if(this[0]) {
-        		var el = $(this[0]);
-				if(value != undefined)
-				{
-					var currentValue = el.plVal();
-					var returnValue = $(this).plVal(value);
-					if(el.hasClass(opts.activeClass) && currentValue == el.attr('placeholder')){
-						el.removeClass(opts.activeClass);
-					}
-					return returnValue;
-				}
-
-        		if(el.hasClass(opts.activeClass) && el.plVal() == el.attr('placeholder')) {
-           			return '';
-       			} else {
-           	 		return el.plVal();
-        		}
-    		}
-    		return undefined;
-		};
-		
-		// Clear placeholder values upon page reload
-		$(window).bind('beforeunload.placeholder', function() {
-			$('input.' + opts.placeholderBlur).val('').attr('autocomplete','off');
-		});
-
-
+			
         return this.each(function() {
             var $el = $(this);
 
