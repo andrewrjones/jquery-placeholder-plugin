2b1df842d6d907055e3a88a9eaf5e6873ab69b90
Andrew Jones <andrew@arjones.co.uk>
2012-10-03 13:23:27 +0100

    use grunt-bump

--- grunt.js
+++ grunt.js
@@ -107,6 +107,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-bump');
   grunt.loadNpmTasks('grunt-css');
   grunt.loadNpmTasks('grunt-jade');
 };
