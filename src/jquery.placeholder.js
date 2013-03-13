e8603baa50d501afda6a539409b10d162f9f20c8
Jacob Bolton <jacob@vervecreations.com>
2013-03-13 15:18:32 -0400

    Added password input field support.
    Tested in IE8/IE7

--- src/jquery.placeholder.js
+++ src/jquery.placeholder.js
@@ -53,7 +53,7 @@
 
     // we don't have to do anything if the browser supports placeholder
     if (!opts.overrideSupport && $.support.placeholder) {
-      return this;
+      //return this;
     }
 
     return this.each(function () {
@@ -64,20 +64,53 @@
         return;
       }
 
-      // we cannot do password fields, but supported browsers can
-      if ($el.is(':password')) {
-        return;
-      }
-
-      // Prevent values from being reapplied on refresh
+    // Prevent values from being reapplied on refresh
       if (opts.preventRefreshIssues) {
         $el.attr('autocomplete', 'off');
+      }
+
+      // duplicate password field with fake text field
+      if ($el.is(':password')) {
+    // var check if a placeholder input exists before trying to create another
+    if ((!this.id) || (!document.getElementById(this.id + '--placeholder'))) {
+      var $placeholderInput = $("<input type='text' />");
+      
+      // copy the attributes over
+      var attrObj = {};
+      for (var i = this.attributes.length - 1; i >= 0; i--) {
+        var thisAttr = this.attributes[i];
+        if (thisAttr.specified && thisAttr.nodeName && (thisAttr.nodeName !== 'type')) {
+          attrObj[thisAttr.nodeName] = thisAttr.nodeValue;
+        }
+      }
+
+      // set default id if none exists
+      var thisId = $.trim($(this).attr('id'));
+      if (thisId === '') thisId = new Date().getTime();
+      attrObj['id'] = thisId + '--placeholder';
+      attrObj['data-linkedid'] = thisId;
+      // set id with placeholder tag
+      $placeholderInput
+        .attr(attrObj)
+        .addClass('placeholderPassword')
+        .insertBefore(this);
+
+      if ($.trim(this.value) === '') {
+        $placeholderInput.show();
+        $(this).hide();
+      }
+    }
       }
 
       $el.bind('focus.placeholder', function () {
         var $el = $(this);
         if (this.value === $el.attr('placeholder') && $el.hasClass(opts.activeClass)) {
           $el.val('').removeClass(opts.activeClass).addClass(opts.focusClass);
+        }
+        // password field handler
+        if ($el.hasClass('placeholderPassword')) {
+          $el.hide();
+          $('#' + $el.data('linkedid')).show().focus();
         }
       });
 
@@ -88,6 +121,15 @@
 
         if (this.value === '') {
           $el.val($el.attr('placeholder')).addClass(opts.activeClass);
+          // password field handler
+          if ($el.is(':password')) {
+            var $placeholderInput = $('#' + $el.attr('id') + '--placeholder');
+            if ($placeholderInput.length) {
+              $el.hide();
+              $placeholderInput.show();
+            }
+            
+          }
         }
       });
 
